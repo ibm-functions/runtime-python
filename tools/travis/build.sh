298d449dc9cb6fe2fbec56e9833ce2116d120c9c
Dave Cariello <drcariel@us.ibm.com>
2018-06-28 15:37:17 -0500

    include admin install

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -27,7 +27,8 @@ TERM=dumb ./gradlew \
 :common:scala:install \
 :core:controller:install \
 :core:invoker:install \
-:tests:install
+:tests:install \
+:tools:admin:install
 
 # Build IBM nodejs runtime
 cd $ROOTDIR
