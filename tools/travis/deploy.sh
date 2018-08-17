a67c80ff66778db9e83ceee95276ce302c1ed6d7
Belinda Vennam <beemarie88@gmail.com>
2018-08-17 07:45:54 -0500

    properties.yml (#55)

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -20,6 +20,7 @@ $ANSIBLE_CMD couchdb.yml
 $ANSIBLE_CMD initdb.yml
 $ANSIBLE_CMD wipe.yml
 $ANSIBLE_CMD openwhisk.yml -e cli_installation_mode=remote
+$ANSIBLE_CMD properties.yml
 
 docker images
 docker ps
@@ -39,5 +40,5 @@ WHISK_APIHOST="172.17.0.1"
 WHISK_AUTH=`cat ${WHISKDIR}/ansible/files/auth.guest`
 WHISK_CLI="${WHISKDIR}/bin/wsk -i"
 
-${WHISK_CLI} property set --apihost ${WHISK_APIHOST} --auth ${WHISK_AUTH} 
+${WHISK_CLI} property set --apihost ${WHISK_APIHOST} --auth ${WHISK_AUTH}
 ${WHISK_CLI} property get
