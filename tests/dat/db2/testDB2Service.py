18e7e0d11ce9ac77da8b7edb36b88f68915eb56b
Falk Zoll <FALKZOLL@de.ibm.com>
2020-01-14 17:04:41 +0100

    Update credentials.json.enc and testDB2Service.py.

--- tests/dat/db2/testDB2Service.py
+++ tests/dat/db2/testDB2Service.py
@@ -11,8 +11,18 @@ def main(args):
         print ("Connection failed.")
         return {"error":"Error connecting to db2"}
 
-        # Select from TestTable (the row just inserted)
-    cmd = "SELECT HISP_DESC FROM SAMPLES.HISPANIC_ORIGIN WHERE HISP_CODE='03'"
+    # Select from TestTable (the row just inserted).
+    #
+    # Due to the db2 lite plan we use, the schema name is fixed to be
+    # the same as the username. We cut the value of the UID field
+    # in the ssldsn to get it and insert it in the select statement.
+    # The ssldsn looks like this:
+    # ssldsn="DATABASE=BLUDB;HOSTNAME=dashdb-xxxx.services.dal.bluemix.net;PORT=50001;PROTOCOL=TCPIP;UID=yyyyyyy;PWD=<hidden>;"
+    #
+    ssldsndict = dict(x.split("=") for x in ssldsn.rstrip(";").split(";"))
+    print("user={}".format(ssldsndict["UID"]))
+
+    cmd = "SELECT HISP_DESC FROM {}.HISPANIC_ORIGIN WHERE HISP_CODE='03'".format(ssldsndict["UID"])
     result = ibm_db.exec_immediate(conn, cmd)
 
     if not result:
