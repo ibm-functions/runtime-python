a550783c16b36f0a63034d1ae37549ae1a624181
Luke-Roy-IBM <83647952+Luke-Roy-IBM@users.noreply.github.com>
2021-06-14 11:53:03 +0200

    Set fixed apache/openwhisk commit (#145)
    - Use a fixed apache/openwhisk commit to run the tests, to explicitly control when changes are consumed.
    - Update the notice year (#5122)

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -9,10 +9,15 @@ HOMEDIR="$SCRIPTDIR/../../../"
 
 # clone openWhisk
 cd $HOMEDIR
-git clone --depth=1 https://github.com/apache/openwhisk.git openwhisk
+# git clone --depth=1 https://github.com/apache/openwhisk.git openwhisk
+git clone https://github.com/apache/openwhisk.git openwhisk
+cd openwhisk
+# Use a fixed commit to run the tests, to explicitly control when changes are consumed.
+# Commit:  Update the notice year (#5122) 
+git checkout ecb2a980659f28d0adbd9ef837afaf4cb2b695bf
+
 
 # setup the openwhisk environment
-cd openwhisk
 ./tools/travis/setup.sh
 
 # Also build required test artifacts
