f28d71118a225d4ce1bbb164989e7766a007ae71
Luke Roy <luke.roy@ibm.com>
2022-07-25 13:40:33 +0200

    update components for build and setup

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -20,10 +20,10 @@ fi
 cd $WHISKDIR
 
 # Pull down images
-docker pull openwhisk/controller:nightly
-docker tag openwhisk/controller:nightly ${IMAGE_PREFIX}/controller
-docker pull openwhisk/invoker:nightly
-docker tag openwhisk/invoker:nightly ${IMAGE_PREFIX}/invoker
+docker pull ibmfunctions/controller:nightly
+docker tag ibmfunctions/controller:nightly ${IMAGE_PREFIX}/controller
+docker pull ibmfunctions/invoker:nightly
+docker tag ibmfunctions/invoker:nightly ${IMAGE_PREFIX}/invoker
 docker pull openwhisk/nodejs6action:nightly
 docker tag openwhisk/nodejs6action:nightly nodejs6action
 docker pull openwhisk/python2action
