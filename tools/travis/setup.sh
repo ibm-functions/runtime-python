d13283532ad46d1b607156ff54d672f7c7ed1cfa
Luke-Roy-IBM <83647952+Luke-Roy-IBM@users.noreply.github.com>
2023-04-13 10:05:07 +0200

    github action Update (#236)
    * use Ubuntu 20.04, python 3.10 and jdk 11 for build
    * update to new Openwhisk build

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -13,9 +13,8 @@ cd $HOMEDIR
 # Clone and setup openwhisk to have a local test environment.
 git clone  https://github.com/ibm-functions/openwhisk.git openwhisk
 cd openwhisk
-# Use a fixed commit to run the tests, to explicitly control when changes are consumed.
-# Commit: minor version bump of azure-storage-blob to fix builds (#5150)
-git checkout 92f5d870bdecf01ccf0bf46baac582955fdbdfa6
+
+git checkout 8d7f1dfc85b12fda81153c34a540f6c01a4780f4
 
 # setup the openwhisk environment
 ./tools/travis/setup.sh
