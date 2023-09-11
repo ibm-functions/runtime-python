ca4294000c80f22054ea26b768197bd386dcc7e3
Luke-Roy-IBM <83647952+Luke-Roy-IBM@users.noreply.github.com>
2023-09-11 14:29:56 +0200

    Github action fixes (#259)
    - Add docker image cleanup so invoker doesnt run into disk pressure issue
    - Update Openwhisk commit tag
    - Add error check if tests fail to view invoker and controller logs

--- tools/travis/test.sh
+++ tools/travis/test.sh
@@ -28,7 +28,12 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 else
   TERM=dumb ./gradlew :tests:testWithoutCredentials
 fi
-
+echo "error code {$?}"
+if [[ "$?" != "0" ]]; then
+  cat /tmp/wsklogs/invoker0/invoker0_logs.log
+  cat /tmp/wsklogs/controller0/controller0_logs.log
+  exit 1
+fi 
 
 #For some reason there no activations, maybe index not ready
 #${WHISK_CLI} activation get --last
